0307a238fcf21b983124c74bf37da0980e5951ed
devzons <devzons@devzonss-iMac.home>
2019-12-04 17:57:51 -0800

    find unique user ids using lodash'

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,17 +1,34 @@
 import _ from 'lodash'
-
 import jsonPlaceholder from '../apis/jsonPlaceholder'
 
-export const fetchPosts = () => async dispatch => {
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts())
+
+  // const userIds = _.uniq(_.map(getState().posts, 'userId'))
+  // userIds.forEach(id => dispatch(fetchUser(id)))
+
+  _.chain(getState().posts)
+    .map('userId')
+    .uniq()
+    .forEach(id => dispatch(fetchUser(id)))
+    .value()
+}
+
+export const fetchPosts = () => async (dispatch, getState) => {
   const response = await jsonPlaceholder.get('/posts')
 
   dispatch({ type: 'FETCH_POSTS', payload: response.data })
 }
 
-export const fetchUser = id => dispatch => _fetchUser(id, dispatch)
-
-const _fetchUser = _.memoize(async (id, dispatch) => {
+export const fetchUser = id => async dispatch => {
   const res = await jsonPlaceholder.get(`/users/${id}`)
 
   dispatch({ type: 'FETCH_USER', payload: res.data })
-})
+}
+// export const fetchUser = id => dispatch => _fetchUser(id, dispatch)
+
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const res = await jsonPlaceholder.get(`/users/${id}`)
+
+//   dispatch({ type: 'FETCH_USER', payload: res.data })
+// })
